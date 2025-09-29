0a04636e6dd3d1c12fa0fa8f6aec5b717445df85
Daria Khrushch <developer.daria.khrushch@gmail.com>
2025-09-29 15:03:13 +0100

    create splash screen

--- ios/jobbee/AppDelegate.swift
+++ ios/jobbee/AppDelegate.swift
@@ -3,6 +3,8 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import RNBootSplash
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -46,5 +48,10 @@ class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
 #else
     Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
+  }
+
+    override func customize(_ rootView: RCTRootView) {
+    super.customize(rootView)
+    RNBootSplash.initWithStoryboard("BootSplash", rootView: rootView) // ⬅️ initialize the splash screen
   }
 }
